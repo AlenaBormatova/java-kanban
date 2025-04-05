05564ca7b15411384b2fff363457ab73514f4bf9
Alena Bormatova <alena.petryuk@yandex.ru>
2025-04-05 22:13:31 +0700

    Добавлены пустые строки между методами для CheckStyle

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -9,29 +9,42 @@ import java.util.List;
 public interface TaskManager {
 
     List<Task> getAllTasks();
+
     List<Epic> getAllEpics();
+
     List<SubTask> getAllSubTasks();
 
     void deleteAllTasks();
+
     void deleteAllEpics();
+
     void deleteAllSubTasks();
 
     Task getTaskById(int id);
+
     Epic getEpicById(int id);
+
     SubTask getSubTaskById(int id);
 
     void addTask(Task task);
+
     void addEpic(Epic epic);
+
     void addSubTask(SubTask subtask);
 
     void updateTask(Task task);
+
     void updateEpic(Epic epic);
+
     void updateSubTask(SubTask subtask);
 
     void deleteTaskById(int id);
+
     void deleteEpicById(int id);
+
     void deleteSubTaskById(int id);
 
     List<SubTask> getSubTasksByEpicId(int epicId);
+    
     List<Task> getHistory();
 }
